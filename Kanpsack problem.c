96c4848bc3cef22f6ab4b8533c6a76cffb23003e
Rohit R M <163559461+Rohit-R-M@users.noreply.github.com>
2024-12-20 00:11:50 +0530

    Kanpsack problem.c

--- Kanpsack problem.c
+++ Kanpsack problem.c
@@ -14,11 +14,11 @@ void knapSack(int w, int wt[], int val[], int n)
   	    for(int j=0;j<=w;j++)
 		{    
 			if(i==0 || j==0)  
-        		k[i][j]=0;  
-      		else if(wt[i-1]<=j)  
-        		k[i][j]=max(val[i-1]+k[i-1][j-wt[i-1]], k[i-1][j]);  
-      		else  
-        		k[i][j]=k[i-1][j];  
+        			k[i][j]=0;  
+      			else if(wt[i-1]<=j)  
+        			k[i][j]=max(val[i-1]+k[i-1][j-wt[i-1]], k[i-1][j]);  
+      			else  
+        			k[i][j]=k[i-1][j];  
     		}  
  	}
 	printf("\nKnapsack matrix:\n");  
